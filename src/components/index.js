ad4f491fbcf84a33287b673b629b0f7b7781badf
yumna1623 <yumnanasir1623@gmail.com>
2025-06-10 03:00:36 +0500

    final

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,6 @@ import Hero from "./Hero";
 import About from "./About";
 import Skills from "./Skills";
 import Contact from "./Contact";
-import Projects from  "./projects";
+export { default as Projects } from './Projects';
 
-export {NavBar , Hero , About , Skills , Contact, Projects};
+export {NavBar , Hero , About , Skills , Contact};
